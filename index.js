cb124dac5634e4edb3a7cf7a9d6a6cef13b0f355
rahulrm00 <141360020+rahulrm00@users.noreply.github.com>
2024-12-14 01:02:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,39 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 const app = express();
 const server = http.createServer(app);
-const io =new Server(server,{
-    cors:{
-        origin: "https://client-ivory-tau.vercel.app/",
-        methods:["GET","POST"],
+const io = new Server(server, {
+    cors: {
+        origin: ["https://client-ivory-tau.vercel.app", "http://localhost:3000"],
+        methods: ["GET", "POST"],
     },
-    transports: ["websocket", "polling"], 
+    transports: ["websocket", "polling"],
 });
+const messageroom ={};
 app.use(cors());
 io.on("connection",(socket)=>{
     console.log(socket.id);
-socket.on("send",(data)=>{
-    socket.broadcast.emit("allmsg",data);
+socket.on("send",({room,message})=>{
+    const  newmsg = message;
+    if(!messageroom[room]){
+        messageroom[room] = [];
+    }
+    messageroom[room].push(newmsg);
+    io.to(room).emit("newmsg",newmsg);
 })
+socket.on("joinroom",(room)=>{
+    socket.join(room);
+    if(messageroom[room]){
+        socket.emit("allmsg",messageroom[room]);
+    }
+})
+socket.on("leaveroom",(room)=>{
+socket.leave(room);
+}
+)
 socket.on("disconnect",()=>{
     console.log("Client disconnected");
 })
 });
-server.listen(4000,()=>{
-    console.log("Server is running on port 4000");
+server.listen(5002,()=>{
+    console.log("Server is running on port 5002");
 });
